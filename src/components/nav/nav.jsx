217219644868e5a41414c4e123eec6ecc1dac264
Arthur40480 <arthurgibert@free.fr>
2023-07-18 18:43:22 +0200

    Add button/menu nav

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./nav.css"
+import croix from "../../../public/croix.svg"
+
+function Nav() {
+    
+    const [isOpen, setIsOpen] = useState(false)
+
+    function openOrCloseNav() {
+        const navItems = document.querySelectorAll(".menu-nav li");
+        setIsOpen(!isOpen);
+
+        navItems.forEach((item) => {
+            if(isOpen === false) {
+                item.classList.add("li-animation");
+              }else {
+                item.classList.remove("li-animation");
+              }
+        })
+    }
+    
+    return (
+        <>
+            <nav className="nav" 
+            style={{right: isOpen ? "0px" : "-100%" }}>
+                <img className="close-nav" src={croix} onClick={openOrCloseNav} alt="Image de croix"/>
+                <h2 className="title-nav">Pierre Gibert Photographie</h2>
+                <ul className="menu-nav">
+                    <li><NavLink exact to="/" activeClassName="active" className="nav-link">Accueil</NavLink></li>
+                    <li><NavLink to="/gallerie" activeClassName="active" className="nav-link">Gallerie</NavLink></li>
+                    <li><NavLink to="/liens" activeClassName="active" className="nav-link">Liens utiles</NavLink></li>
+                    <li><NavLink to="/contact" activeClassName="active" className="nav-link">Contact</NavLink></li>
+                    <li><NavLink to="/mention" activeClassName="active" className="nav-link">Mention légale</NavLink></li>
+                </ul>
+            </nav>
+            <div onClick={openOrCloseNav} className="trigger-nav"
+            style={{display: isOpen ? "none" : "flex"}}>
+                <div className="element-nav">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Nav;
