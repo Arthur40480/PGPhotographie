44c299b24ba60ed4aafff01ba125c73ce9115c69
Arthur40480 <arthurgibert@free.fr>
2023-07-24 14:46:59 +0200

    Home page content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,24 @@
 import "./Home.css"
-import Mouse from "../../components/mouse/mouse";
 import WelcomSection from "../../components/welcomSection/welcomSection";
-
+import img3 from "../../assets/accueil_img_3.jpg"
 
 function Home() {
     return (
         <main className="home-main">
-            <Mouse />
             <WelcomSection />
+            <section className="news-section">
+                <h2 className="news-title">Actualités</h2>
+                <div className="news-content">
+                    <h3>Titre</h3>
+                    <p>Lieu - date</p>
+                    <p>Description</p>
+                </div>
+            </section>
+            <div className="home-cover">
+                <div className="home-cover-container">
+                    <img className="home-cover-img" src={img3} alt="Photo de paysage" />
+                </div>
+            </div>
         </main>
     )
 }
