915d2757a48af7becb84dbecfbd22f116efeb4bd
Arthur40480 <arthurgibert@free.fr>
2023-09-19 22:06:08 +0200

    Carousel changes

--- frontend/src/components/carrousel/carrousel.jsx
+++ frontend/src/components/carrousel/carrousel.jsx
@@ -7,7 +7,6 @@ function Carrousel({ onClose, data, selectedImg }) {
     
     const [ imgCurrent, setImgCurrent ] = useState(selectedImg);
     const [isImageTransitioning, setImageTransitioning] = useState(false);
-    console.log(imgCurrent);
 
     const selectedData = data[imgCurrent];
     const lengthData = data.length;
@@ -31,18 +30,9 @@ function Carrousel({ onClose, data, selectedImg }) {
     const handleImageClick = (index) => {
         setImageTransitioning(true);
         setTimeout(() => {
-            // Calculez le nouvel indice en ajoutant simplement l'indice de la miniature cliquée à imgCurrent.
-            let newIndex = imgCurrent + index;
-
-             // Assurez-vous que le nouvel indice reste dans la plage valide de 0 à lengthData - 1.
-            if (newIndex < 0) {
-                newIndex += lengthData;
-            } else if (newIndex >= lengthData) {
-                newIndex -= lengthData;
-            }
-            setImgCurrent(newIndex);
+            setImgCurrent(i => i < lengthData - 1 ? i + index : i + index - (lengthData - 1));
             setImageTransitioning(false);
-        }, 300);
+        }, 300); 
     };
 
     return (
