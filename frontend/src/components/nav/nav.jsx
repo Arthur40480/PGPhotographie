f20dfacbf30f3e42a8e6fce15eda6801f98bf364
Arthur40480 <arthurgibert@free.fr>
2023-09-21 14:11:12 +0200

    Added link icons for the nav

--- frontend/src/components/nav/nav.jsx
+++ frontend/src/components/nav/nav.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./nav.css";
 import croix from "../../../public/croix.svg";
+import iconBook from "../../../public/book.svg"
+import iconHome from "../../../public/home.svg"
+import iconContact from "../../../public/contact.svg"
+import iconGallery from "../../../public/gallery.svg"
 
 function Nav() {
 
@@ -34,10 +38,22 @@ function Nav() {
             style={{right: isOpen ? "0px" : "-100%" }}>
                 <img className="close-nav" src={croix} onClick={openOrCloseNav} alt="Image de croix"/>
                 <ul className="menu-nav">
-                    <li><NavLink exact to="/" activeClassName="active" className="nav-link">Accueil</NavLink></li>
-                    <li><NavLink to="/category" activeClassName="active" className="nav-link">Galerie</NavLink></li>
-                    <li><NavLink to="/golden-book" activeClassName="active" className="nav-link">Livre d&rsquo;Or</NavLink></li>
-                    <li><a href={`mailto:${email}`} className="nav-link">Contact</a></li>                    
+                    <li>
+                        <NavLink exact to="/" activeClassName="active" className="nav-link">Accueil</NavLink>
+                        <img src={iconHome} className="icon-nav" alt="Icone de maison pour l'accueil"/>
+                    </li>
+                    <li>
+                        <NavLink to="/category" activeClassName="active" className="nav-link">Galerie</NavLink>
+                        <img src={iconGallery} className="icon-nav" alt="Icone de photo pour gallerie"/>
+                    </li>
+                    <li>
+                        <NavLink to="/golden-book" activeClassName="active" className="nav-link">Livre d&rsquo;Or</NavLink>
+                        <img src={iconBook} className="icon-nav" alt="Icone de livre pour livre d'or"/>
+                    </li>
+                    <li>
+                        <a href={`mailto:${email}`} className="nav-link">Contact</a>
+                        <img src={iconContact} className="icon-nav" alt="Icone de message pour contact"/>
+                    </li>                    
                 </ul>
             </nav>
             <div className="black-box" onClick={openOrCloseNav} style={{display: isOpen ? "flex" : "none"}}>
