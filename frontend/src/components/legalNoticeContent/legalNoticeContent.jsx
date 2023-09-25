e879f128c0809b79586018eadd3b77956122f1f1
Arthur40480 <arthurgibert@free.fr>
2023-09-25 13:21:43 +0200

    Add the legal notice component

---
+++ frontend/src/components/legalNoticeContent/legalNoticeContent.jsx
@@ -0,0 +1,11 @@
+import "./legalNoticeContent.css";
+
+function legalNoticeContent() {
+    return (
+        <>
+        <section className="legal-notice-section"></section>
+        </>
+    )
+}
+
+export default legalNoticeContent;
--- frontend/src/pages/LegalNotice/LegalNotice.jsx
+++ frontend/src/pages/LegalNotice/LegalNotice.jsx
@@ -1,9 +1,13 @@
 import "./LegalNotice.css";
+import legalNoticeContent from "../../components/legalNoticeContent/legalNoticeContent";
 
 function LegalNotice() {
     return (
         <main className="legal-notice-main">
-            
+            <div className="legal-notice-container-title">
+                <h1 className="legal-notice-title"><span>Mentions légales</span></h1>
+            </div>
+            <legalNoticeContent />
         </main>
     )
 }
