4f75d79730cb32ee95080e26937fa2be3ae671de
Arthur40480 <arthurgibert@free.fr>
2023-10-01 19:53:05 +0200

    Addition of the photo ID in the data table, and modification on the carousel

--- frontend/src/components/carrousel/carrousel.jsx
+++ frontend/src/components/carrousel/carrousel.jsx
@@ -5,42 +5,40 @@ import { useState } from "react";
 import PropTypes from 'prop-types';
 
 function Carrousel({ onClose, data, selectedImg }) {
-    
-    const [ imgCurrent, setImgCurrent ] = useState(selectedImg);
+
     const [isImageTransitioning, setImageTransitioning] = useState(false);
-
-    const selectedData = data[imgCurrent];
-    const lengthData = data.length;
-
-    function ShowNextImg() {
+    const [currentIndex, setCurrentIndex] = useState(
+        data.findIndex((image) => image.id === selectedImg)
+    );
+    
+    const handleNextImage = () => {
         setImageTransitioning(true);
         setTimeout(() => {
-            setImgCurrent(imgCurrent === lengthData - 1 ? 0 : imgCurrent + 1);
+            setCurrentIndex((prevIndex) =>
+            prevIndex === data.length - 1 ? 0 : prevIndex + 1
+            );
             setImageTransitioning(false);
         }, 300);
-    }
-    
-    function ShowPreviousImg() {
-        setImageTransitioning(true);
-        setTimeout(() => {
-            setImgCurrent(imgCurrent === 0 ? lengthData - 1 : imgCurrent - 1);
-            setImageTransitioning(false);
-        }, 300); 
-    }
-
-    const handleImageClick = (index) => {
-        setImageTransitioning(true);
-        setTimeout(() => {
-            setImgCurrent(i => i < lengthData - 1 ? i + index : i + index - (lengthData - 1));
-            setImageTransitioning(false);
-        }, 300); 
     };
+    
+    const handlePreviousImage = () => {
+        setImageTransitioning(true);
+        setTimeout(() => {
+            setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? data.length - 1 : prevIndex - 1
+            );
+            setImageTransitioning(false);
+        }, 300);
+
+    };
+    
+    const selectedData = data[currentIndex];
 
     return (
         <>
             <div className="popup-container">
                 <img src={buttonClose} className="button-close-popup" onClick={onClose} alt="Button pour fermer le carrousel" />
-                <p className="paging">{imgCurrent + 1}/{data.length}</p>
+                <p className="paging">{currentIndex + 1}/{data.length}</p>
                 <div className="carrousel-container"> 
                     <img src={selectedData.url} className={`photo-carrousel ${isImageTransitioning ? "transitioning" : ""}`} alt={`Photo ${selectedData.legend}`} />
                 </div>
@@ -48,39 +46,8 @@ function Carrousel({ onClose, data, selectedImg }) {
                     <p className="legend">{selectedData.legend}</p>
                 </div>
                 <div className="thumbnail-carrousel">
-                    <img src={buttonNext} className="button-arrow-carrousel previous" onClick={ShowPreviousImg} alt="Boutton pour parcourir les photos" />
-                {lengthData >= 5 && (
-                    data.slice(imgCurrent, imgCurrent + 5).map((photo, index) => (
-                        <div 
-                        className={`thumbnail-container ${index === 0 ? 'current-thumbnail' : ''}`}
-                        key={index}
-                        onClick={() => handleImageClick(index)}>
-                            <img src={photo.url} alt={photo.legend} />
-                        </div>
-                    ))
-                )}
-                {imgCurrent + 5 > lengthData && lengthData >= 5 && (
-                    data.slice(0, imgCurrent + 5 - lengthData).map((photo, index) => (
-                        <div className="thumbnail-container" key={index} onClick={() => handleImageClick(index)}>
-                            <img src={photo.url} alt={photo.legend} />
-                        </div>
-                    ))
-                )}
-                {lengthData < 5 && (
-                    data.slice(imgCurrent, lengthData).map((photo, index) => (
-                        <div className={`thumbnail-container ${index === 0 ? 'current-thumbnail' : ''}`} key={index} onClick={() => handleImageClick(index)}>
-                            <img src={photo.url} alt={photo.legend}/>
-                        </div>
-                    ))
-                )}
-                {imgCurrent + lengthData > lengthData && lengthData < 5 && (
-                    data.slice(0, imgCurrent ).map((photo, index) => (
-                        <div className="thumbnail-container" key={index} onClick={() => handleImageClick(index)}>
-                            <img src={photo.url} alt={photo.legend}/>
-                        </div>
-                    ))
-                )}
-                    <img src={buttonNext} className="button-arrow-carrousel next" onClick={ShowNextImg} alt="Boutton pour parcourir les photos" />
+                    <img src={buttonNext} className="button-arrow-carrousel previous" onClick={handlePreviousImage} alt="Boutton pour parcourir les photos" />
+                    <img src={buttonNext} className="button-arrow-carrousel next" onClick={handleNextImage} alt="Boutton pour parcourir les photos" />
                 </div>
             </div>
         </>
