2cd5a498be5fe667829c2ae3b172b8a5348990b1
Arthur40480 <arthurgibert@free.fr>
2023-10-24 16:16:23 +0200

    Deleting Home Photo

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import "./Home.css";
 import WelcomSection from "../../components/welcomSection/welcomSection";
 import News from "../../components/news/news";
-import img3 from "../../assets/accueil_img_3.jpg";
 
 function Home() {
 
@@ -9,11 +8,6 @@ function Home() {
         <main className="home-main">
             <WelcomSection />
             <News />
-            <div className="home-cover">
-                <div className="home-cover-container">
-                    <img className="home-cover-img" src={img3} alt="Photo de l'accueil 3 : paysage" />
-                </div>
-            </div>
         </main>
     )
 }
