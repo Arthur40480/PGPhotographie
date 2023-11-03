973902c230a709d96d550ea899cae2556cf304d7
Arthur40480 <arthurgibert@free.fr>
2023-11-03 20:18:06 +0100

    Deleting welcome photos finish

--- frontend/src/components/welcomSection/welcomSection.jsx
+++ frontend/src/components/welcomSection/welcomSection.jsx
@@ -1,23 +1,13 @@
 import "./welcomSection.css";
-import img1 from "../../assets/accueil_img_1.jpg";
-import img2 from "../../assets/accueil_img_2.jpg";
 import CategoryButton from "../categoryButton/categoryButton";
 
 function WelcomSection() {
     return(
         <>
             <section className="welcom-section">
-                <div className="welcom-container-img">
-                    <img className="welcom-img" src={img1} alt="Photo d'accueil 1 : Une corbeille de fruit"/>
-                </div>
                 <div className="content-container">
-                    <div className="content">
-                        <p>C&lsquo;est la faute aux photons si la photo me botte ...</p>
-                    </div>
+                    <p className="content">C&lsquo;est la faute aux photons si la photo me botte ...</p>
                     <CategoryButton />
-                </div>
-                <div className="welcom-container-img">
-                    <img className="welcom-img" src={img2} alt="Photo d'accueil 2 : paysage"/>
                 </div>
             </section>
         </>
