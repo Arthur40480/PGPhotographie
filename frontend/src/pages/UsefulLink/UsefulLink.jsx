3619136c9b6313567d65bfed3650324922b30dc7
Arthur40480 <arthurgibert@free.fr>
2023-11-09 21:05:23 +0100

    Added title usefull link

--- frontend/src/pages/UsefulLink/UsefulLink.jsx
+++ frontend/src/pages/UsefulLink/UsefulLink.jsx
@@ -1,6 +1,10 @@
 function UsefulLink() {
     return (
-        <></>
+        <main className="usefull-link-main">
+            <div className="container-usefull-link-title">
+                <h1 id="usefull-link" className="usefull-link-title"><span>Liens utiles</span></h1>
+            </div>
+        </main>
     )
 }
 
