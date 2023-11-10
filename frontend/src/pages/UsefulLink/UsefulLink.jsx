6c861e0e546b8ed55a59cf0ea273e31775ce0f7b
Arthur40480 <arthurgibert@free.fr>
2023-11-10 11:34:26 +0100

    Adding the link to the useful links page in the nav

--- frontend/src/pages/UsefulLink/UsefulLink.jsx
+++ frontend/src/pages/UsefulLink/UsefulLink.jsx
@@ -1,8 +1,8 @@
 function UsefulLink() {
     return (
-        <main className="usefull-link-main">
-            <div className="container-usefull-link-title">
-                <h1 id="usefull-link" className="usefull-link-title"><span>Liens utiles</span></h1>
+        <main className="useful-link-main">
+            <div className="container-useful-link-title">
+                <h1 id="useful-link" className="useful-link-title"><span>Liens utiles</span></h1>
             </div>
         </main>
     )
